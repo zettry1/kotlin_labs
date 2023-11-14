25e6c3186b20df9c3c55d2203ec3550446ac2110
B.Orgilbat <zettry1@gmail.com>
2023-11-13 19:36:34 -0600

    feat: added product model

---
+++ app/src/main/java/com/example/miu_mobile_assignemnt/model/Product.kt
@@ -0,0 +1,9 @@
+package com.example.miu_mobile_assignemnt.model
+
+data class Product (
+    val productName: String,
+    val productDescription: String,
+    val cost: Double,
+    val image: Int,
+    val brandLogo: Int,
+)
